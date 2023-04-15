2cacede052d7dc0e45df1aafea331c687bf7dda2
shivrjpatil <shivrajp972@gmail.com>
2023-04-15 19:48:41 +0530

    Added program to print Table of power of two

---
+++ ProgrammingConstructs/PowerofTwo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstructs
+{
+    internal class PowerofTwo
+    {
+        public void Power()
+        {
+            Console.Write("Enter the value of n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Powers of 2 less than or equal to 2^n:");
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine($"2^{i} = {Math.Pow(2, i)}");
+            }
+        }
+    }
+}
--- ProgrammingConstructs/Program.cs
+++ ProgrammingConstructs/Program.cs
@@ -11,7 +11,7 @@ namespace ProgrammingConstructs
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Programming constructs Programs");
-            Console.WriteLine("1.Two Number Equal or Not\n2.Even or Odd\n3.Candidate eligible For voting\n4.Largest of Three numbers\n5.Eligibility for admission\n6.Week of the day\n7.Arithmetic Operation");
+            Console.WriteLine("1.Two Number Equal or Not\n2.Even or Odd\n3.Candidate eligible For voting\n4.Largest of Three numbers\n5.Eligibility for admission\n6.Week of the day\n7.Arithmetic Operation\n8.Power of two");
             int option=Convert.ToInt32(Console.ReadLine());
 
             switch(option)
@@ -43,6 +43,10 @@ namespace ProgrammingConstructs
                 case 7:
                     AirthmaticOperation arithmetic=new AirthmaticOperation();
                     arithmetic.Airthmatic();
+                    break;
+                case 8:
+                    PowerofTwo powerofTwo = new PowerofTwo();
+                    powerofTwo.Power();
                     break;
             }
 
